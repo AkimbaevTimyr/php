122e92f1c10dfb06e04182aa6f68a26aa9a8236f
AkimbaevTimyr <60063664+AkimbaevTimyr@users.noreply.github.com>
2023-03-31 10:23:56 +0300

    Create in Router.php upperCamelCase, lowerCamelCase, matchRoute functions

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -26,7 +26,59 @@ class Router
 
     public static function dispatch($url)
     {
-        var_dump($url);
+        if(self::matchRoute($url))
+        {
+            echo 'Okey';
+        } else {
+            echo 'No';
+        }
+    }
+    public static function matchRoute($url): bool
+    {
+        //в pattern попадает шаблон регулярного выражения
+        //в route попадает массив в путям
+        //флаг i делет шаблон регистро независемым
+        foreach (self::$routes as $pattern => $route)
+        {
+            if(preg_match("#{$pattern}#", $url, $matches)){
+                //получили массив matcher, перебираем его и пути добавляем в массив route
+                foreach ($matches as $key => $value){
+                    if(is_string($key)) {
+                        $route[$key] = $value;
+                    }
+                }
+                //если у нас нет action в запросе то мы добавляем дефолтный action
+                if(empty($route['action'])){
+                    $route['action'] = 'index';
+                }
+                if(!isset($route['admin_prefix'])){
+                    $route['admin_prefix'] = "";
+                }else{
+                    $route['admin_prefix'] = '\\';
+                }
+                $route['controller']  = self::upperCamelCase($route['controller']);
+                debug($route);
+                return true;
+            }
+        }
+        return false;
     }
 
+    //приводит строку к CamelCase
+    protected static function upperCamelCase($name): string
+    {
+        //new-product => new product
+        $name = str_replace('-', ' ', $name);
+        //new product => New Product
+        $name = ucwords($name);
+        //New Product => NewProduct
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
+
+    //return camelCase
+    protected static function lowerCamelCase($name): string
+    {
+        return lcfirst(self::upperCamelCase($name));
+    }
 }
